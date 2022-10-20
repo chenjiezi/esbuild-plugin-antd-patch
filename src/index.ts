8e891a692f6169c319f3cf5194f509b174c7742c
chenjiezi <892774940@qq.com>
2022-10-20 19:37:01 +0800

    wip: core

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-export const log = (...args: any[]) => {
-  console.log(...args)
+import fs from 'fs'
+import { parse } from '@babel/parser'
+import { transformFromAstSync } from '@babel/core'
+interface esTransformConfig {
+  include: RegExp
 }
+
+function esTransform(config: esTransformConfig) {
+  const { include } = config
+  if (!include)
+    throw new Error('"include" is must.')
+
+  return {
+    name: 'esbuild-plugin-es-transform',
+    setup(build: any) {
+      build.onLoad({ filter: include }, async (args: any) => {
+        console.log('onLoad')
+        const { path } = args
+        const source = fs.readFileSync(path, 'utf-8')
+
+        const ast = parse(source, { sourceType: 'module' })
+
+        // const code = transformFromAstSync(ast, undefined, {
+        //   presets: ['env'],
+        // })
+
+        console.log('ast:', ast)
+
+        return { contents: source }
+      })
+    },
+  }
+}
+
+export default esTransform
