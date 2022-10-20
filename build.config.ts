c1f18b9d3c2f1cce3cc0ebf477d107ac649bc0e7
chenjiezi <892774940@qq.com>
2022-10-20 11:07:33 +0800

    chore: init

---
+++ build.config.ts
@@ -0,0 +1,13 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  entries: [
+    'src/index'
+  ],
+  outDir: 'dist',
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: true,
+  }
+})
