17d641515126b3d094a1233cb3d673839d4c004c
chenjiezi <892774940@qq.com>
2022-10-22 09:46:15 +0800

    feat:  function default parameters

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,13 @@ interface antdPatchConfig {
 }
 
 function antdPatch(config: antdPatchConfig) {
-  const { include } = config
-  if (!include)
-    throw new Error('"include" is must.')
+  const { include = /moment-util\.js$/ } = config
 
   return {
     name: 'esbuild-plugin-antd-patch',
     setup(build: { onLoad: (arg0: { filter: RegExp }, arg1: (args: { path: fs.PathOrFileDescriptor }) => Promise<{ contents: string }>) => void }) {
       build.onLoad({ filter: include }, async (args: { path: fs.PathOrFileDescriptor }) => {
-        let source = fs.readFileSync(args.path, 'utf8')
+        let source = fs.readFileSync(args.path, 'utf-8')
         if (source.indexOf('import * as moment from')) {
           source = source.replace(
             /import\s\*\sas\smoment\sfrom/g,
